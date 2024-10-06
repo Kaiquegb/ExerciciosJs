28849d28ab811261e1a2243df61a53977ed094c6
Kaique Alves <116605416+Kaiquegb@users.noreply.github.com>
2024-10-06 12:11:04 -0300

    Initial commit

---
+++ exercicio1.js
@@ -0,0 +1,20 @@
+/* 
+
+    solução da lista - exercicio 1 
+
+*/
+
+function reverse_a_number(n)
+{    
+    console.log(typeof(n));
+    n = n + "";
+    console.log(typeof(n));
+    console.log(n.split())
+    console.log(n.split(""));
+    console.log(n.split("").reverse())
+    console.log(n.split("").reverse().join(""));
+    return n.split("").reverse().join("");
+   
+}
+
+console.log(reverse_a_number(123456));
